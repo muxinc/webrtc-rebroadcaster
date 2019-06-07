14726091a5245784554e0c55b5fd04cf51119aaa
Nick Chadwick <chadnickbok@gmail.com>
2019-06-06 19:03:02 -0700

    Threads and settings for entrypoint

--- chromium_broadcaster/entrypoint.sh
+++ chromium_broadcaster/entrypoint.sh
@@ -9,7 +9,7 @@ Xvfb :1 -screen 0 1280x720x24 &
 sleep 1
 
 echo "Capturing with FFmpeg"
-ffmpeg -y -f x11grab -draw_mouse 0 -s 1280x720 -r 25 -i :1.0+0,0 -f pulse -ac 2 -i default -c:v libx264 -preset fast -crf 24 -c:a aac -b:a 128k -f flv ${2} &
+ffmpeg -y -f x11grab -draw_mouse 0 -s 1280x720 -r 25 -i :1.0+0,0 -f pulse -ac 2 -i default -c:v libx264 -preset veryfast -threads 3 -crf 24 -maxrate 4000k -bufsize 4000k -c:a aac -b:a 128k -f flv ${2} &
 
 echo "Launching Chromium"
 DISPLAY=:1 DISPLAY=:1.0 chromium-browser --no-sandbox --incognito --disable-gpu --user-data-dir=/tmp/test  --window-position=0,0 --window-size=1280,720 "--app=${1}"

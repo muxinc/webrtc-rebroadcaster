71ba2336c72199e149c023e304fdddda843eecf5
Nick Chadwick <chadnickbok@gmail.com>
2019-06-12 10:19:38 -0700

    Remove projects moved to new homes

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <api/peerconnectioninterface.h>
-#include <iostream>
-
-
-int main(void) {
-    rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> pcfi = webrtc::CreatePeerConnectionFactory(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-    std::cout << "Hello world!\n";
-}
